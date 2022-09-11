5b523c828eeb3420834d839f2821bbf50ae2dfa1
Irfan Shaikh <irfansk.se@gmail.com>
2022-09-11 20:50:46 +0530

    hero section completes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from './components/index'
+import styles from './styles'
 
-const App = () => {
-  return (
-    <div>React with Tailwind</div>
-  )
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="bg-primary w-full overflow-hidden">
+        {/* Navbar */}
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            {' '}
+            <Navbar />
+          </div>
+        </div>
+        {/* Hero Section */}
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+        {/*  */}
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{' '}
+            <Clients /> <CTA /> <Footer />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App
